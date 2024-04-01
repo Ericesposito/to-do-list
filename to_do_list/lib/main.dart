bb55c153d061034fa3fc9e07a96b0c00fdaa5eba
Ericesposito <eespo93@optonline.net>
2024-04-01 19:11:26 -0400

    completed basic to-do item list to learn about managing state and the importance of keys in Flutter behind the scenes.

--- to_do_list/lib/main.dart
+++ to_do_list/lib/main.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_internals/keys/keys.dart';
 
 void main() {
+  const numbers = [1, 2, 3];
+  print(numbers);
+  numbers.add(4);
+  print(numbers);
+  // numbers = [5, 6, 7];
+  // print(numbers);
   runApp(const App());
 }
 
